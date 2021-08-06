9bad1262d2cfcaa410528b91451d29fcfef613bf
Martin Vossen <martin.vossen@sap.com>
2021-08-06 10:44:13 +0200

    fix syntax errors

--- collectors/apicmcpfault.py
+++ collectors/apicmcpfault.py
@@ -28,23 +28,21 @@ class ApicMCPCollector(BaseCollector.BaseCollector):
             fetched_data = self.connection.getRequest(host, query)
             if not self.connection.isDataValid(fetched_data):
                 LOG.warning(
-                    "Skipping apic host %s, %s did not return anything", host,
-                    query)
+                    "Skipping apic host %s, %s did not return anything", host, query)
                 continue
 
             if len(fetched_data['imdata']) == 0:
-                output = json.loads(query.text)
                 # Add Empty Counter to have the metric show up in Prometheus.
                 # Otherwise they only show when something is wrong and we dont know if it is actually working
-                c_mcp_faults.add_metric(labels=[host, '', '', ''], 0)
+                c_mcp_faults.add_metric(labels=[host, '', '', ''], value=0)
                 metric_counter += 1
                 break  # Each host produces the same metrics.
             count = int(fetched_data['totalCount'])
             for x in range(0, int(count)):
-                if (output['imdata'][x]['faultInst']['attributes']['lc'] =='raised') or (output['imdata'][x]['faultInst']['attributes']['lc'] =='soaking'):
-                    fault_lifecyle = output['imdata'][x]['faultInst']['attributes']['lc']
-                    fault_summary = output['imdata'][x]['faultInst']['attributes']['dn']
-                    fault_desc = output['imdata'][x]['faultInst']['attributes']['descr']
+                if (fetched_data['imdata'][x]['faultInst']['attributes']['lc'] =='raised') or (fetched_data['imdata'][x]['faultInst']['attributes']['lc'] =='soaking'):
+                    fault_lifecyle = fetched_data['imdata'][x]['faultInst']['attributes']['lc']
+                    fault_summary = fetched_data['imdata'][x]['faultInst']['attributes']['dn']
+                    fault_desc = fetched_data['imdata'][x]['faultInst']['attributes']['descr']
 
                     LOG.debug("host: %s, ip: %s", fault_lifecyle, fault_summary, fault_desc)
                     metric_counter += 1
