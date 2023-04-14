2d8f70a4d995fa8abdc6760b86f5021dce5772ab
tomas-ronec <60659205+tomas-ronec@users.noreply.github.com>
2023-04-14 14:40:45 +0200

    Fixing issues for remote L3 EPs and changing avg to last

--- collectors/ApicLeafCapacityCollector.py
+++ collectors/ApicLeafCapacityCollector.py
@@ -18,7 +18,7 @@ class ApicLeafCapacityCollector(Collector):
 
     def get_query(self) -> str:
         return '/api/class/eqptcapacityEntity.json?query-target=self' + \
-               '&rsp-subtree-include=stats&rsp-subtree-class=eqptcapacityL3RemoteUsageCap5min,' + \
+               '&rsp-subtree-include=stats&rsp-subtree-class=eqptcapacityL3RemoteUsage5min,' + \
                'eqptcapacityL3TotalUsageCap5min,eqptcapacityL3Usage5min,eqptcapacityL3TotalUsage5min,' + \
                'eqptcapacityL2Usage5min,eqptcapacityL2RemoteUsage5min,eqptcapacityL2TotalUsage5min'
 
@@ -43,30 +43,30 @@ class ApicLeafCapacityCollector(Collector):
                         g_leaf_cap_tcam.add_metric(labels=[leaf_id, 'max', 'l3'],
                                                    value=l3_max)
                     elif 'eqptcapacityL3TotalUsage5min' in data_object:
-                        l3_total = data_object['eqptcapacityL3TotalUsage5min']['attributes']['v4TotalEpAvg']
+                        l3_total = data_object['eqptcapacityL3TotalUsage5min']['attributes']['v4TotalEpLast']
                         g_leaf_cap_tcam.add_metric(labels=[leaf_id, 'total', 'l3'],
                                                    value=l3_total)
                     elif 'eqptcapacityL3Usage5min' in data_object:
-                        l3_local = data_object['eqptcapacityL3Usage5min']['attributes']['v4LocalEpAvg']
+                        l3_local = data_object['eqptcapacityL3Usage5min']['attributes']['v4LocalEpLast']
                         g_leaf_cap_tcam.add_metric(labels=[leaf_id, 'local', 'l3'],
                                                    value=l3_local)
-                    elif 'eqptcapacityL3RemoteUsageCap5min' in data_object:
-                        l3_remote = data_object['eqptcapacityL3RemoteUsageCap5min']['attributes']['v4RemoteEpCapAvg']
+                    elif 'eqptcapacityL3RemoteUsage5min' in data_object:
+                        l3_remote = data_object['eqptcapacityL3RemoteUsage5min']['attributes']['v4RemoteEpLast']
                         g_leaf_cap_tcam.add_metric(labels=[leaf_id, 'remote', 'l3'],
                                                    value=l3_remote)
                     elif 'eqptcapacityL2TotalUsage5min' in data_object:
                         l2_max = data_object['eqptcapacityL2TotalUsage5min']['attributes']['totalEpCapMax']
-                        l2_total = data_object['eqptcapacityL2TotalUsage5min']['attributes']['totalEpAvg']
+                        l2_total = data_object['eqptcapacityL2TotalUsage5min']['attributes']['totalEpLast']
                         g_leaf_cap_tcam.add_metric(labels=[leaf_id, 'max', 'l2'],
                                                    value=l2_max)
                         g_leaf_cap_tcam.add_metric(labels=[leaf_id, 'total', 'l2'],
                                                    value=l2_total)
                     elif 'eqptcapacityL2Usage5min' in data_object:
-                        l2_local = data_object['eqptcapacityL2Usage5min']['attributes']['localEpAvg']
+                        l2_local = data_object['eqptcapacityL2Usage5min']['attributes']['localEpLast']
                         g_leaf_cap_tcam.add_metric(labels=[leaf_id, 'local', 'l2'],
                                                    value=l2_local)
                     elif 'eqptcapacityL2RemoteUsage5min' in data_object:
-                        l2_remote = data_object['eqptcapacityL2RemoteUsage5min']['attributes']['remoteEpAvg']
+                        l2_remote = data_object['eqptcapacityL2RemoteUsage5min']['attributes']['remoteEpLast']
                         g_leaf_cap_tcam.add_metric(labels=[leaf_id, 'remote', 'l2'],
                                                    value=l2_remote)
         return [g_leaf_cap_tcam]
