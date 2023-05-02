e47c93120238d493e9b8e1f53a7281b74d67054d
tomas-ronec <60659205+tomas-ronec@users.noreply.github.com>
2023-05-02 16:59:52 +0200

    adds metric network_apic_faults_severity  (#19)
    * adds new metric 'network_apic_faults_severity' with labels ['severity', 'type', 'domain', 'ack]
    * metric is created for every combination that occurs in the data scraped from APIC, but not all possible combinations
    
    ---------
    
    Co-authored-by: Tomas Ronec <tomas.ronec@sap.com>
    Co-authored-by: IvoGoman <ivo.gosemann@sap.com>

---
+++ collectors/ApicFaultsCollector.py
@@ -0,0 +1,51 @@
+from Collector import Collector
+import logging
+from prometheus_client.core import GaugeMetricFamily
+from typing import List, Dict
+
+LOG = logging.getLogger('apic_exporter.exporter')
+
+
+class ApicFaultsCollector(Collector):
+
+    def __init__(self, config: Dict):
+        super().__init__('apic_faults', config)
+
+    def describe(self):
+        yield GaugeMetricFamily('network_apic_faults',
+                                'APIC faults')
+
+    def get_query(self) -> str:
+        return '/api/node/class/faultInst.json?' + \
+               'query-target-filter=or(eq(faultInst.lc,"raised"),eq(faultInst.lc,"soaking"))'
+
+    def get_metrics(self, host: str, data: Dict) -> List[GaugeMetricFamily]:
+        """Collect and sort APIC Faults by type, severity, domain and acknowledgement"""
+        g_apic_faults = GaugeMetricFamily('network_apic_faults_severity',
+                                          'APIC faults by severity, type and domain',
+                                          labels=['severity', 'type', 'domain', 'ack'])
+
+        faults = {}
+        for fault_object in data['imdata']:
+            try:
+                attrs = unpack(fault_object, 'faultInst', 'attributes')
+                key = tuple(unpack(attrs, x)
+                            for x in ('severity', 'type', 'domain', 'ack'))
+                faults[key] = faults.get(key, 0) + 1
+            except ValueError as e:
+                LOG.error(
+                    f"faultCollector: invalid path {e} in {fault_object}")
+
+        for k, v in faults.items():
+            g_apic_faults.add_metric(labels=k,
+                                     value=v)
+        return [g_apic_faults]
+
+
+def unpack(data, *keys):
+    for k in keys:
+        if isinstance(data, dict) and data.get(k):
+            data = data[k]
+        else:
+            return ValueError(f"{keys} not found in data")
+    return data
